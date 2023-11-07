36419398c65d40b3606dfc6c447ea5dae2c41210
IRFANABU <irfan10942@gmail.com>
2023-11-08 02:36:16 +0530

    commited on 8nov

--- index.php
+++ index.php
@@ -83,14 +83,14 @@
       <img src="image/oneplus-carousel.webp" class="d-block w-100" alt="...">
     </div>
   </div>
-  <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
   <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
-  </button> -->
+  </button>
 </div>
 </section>
 <section class="container-fluid bg-light text-center p-5" id="videoSec">
