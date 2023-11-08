d09e5bc08da872973bedf9d3429fc196a96b86e5
IRFANABU <irfan10942@gmail.com>
2023-11-08 10:53:54 +0530

    commited added new style

--- cartPage.php
+++ cartPage.php
@@ -1,3 +1,18 @@
+<?php
+                    include('server.php');
+                    $sql = "select * from productdetails";
+                    $result = $conn->query($sql);
+                    $conn->close();
+                     if($result){
+                        while($row = mysqli_fetch_array($result)){
+                             $model= $row['modelname'];
+                            $imagep= $row['image'];
+                        }
+                    }
+                  //  echo '<img src=data:image;base64,'.$row['$image'].' />
+                    ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,10 +21,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>cart</title>
 </head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.7.1.min.js"
-    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="Styles/product.css">
@@ -43,7 +56,7 @@
                     </ul>
 
                 </div>
-                <div class="user me-5 d-flex" id="user">
+                <div class="me-5" id="user">
                     <div>
                         <i class="text-small fa fa-search me-3" id="searchIcon"></i>
     
@@ -59,7 +72,7 @@
                 
                 </div>
                 <div class="menu me-2">
-                    <span class="fa fa-bars   me-3 " id="icon"></span>
+                    <span class="fa fa-bars fs-4  me-3 " id="icon"></span>
                 </div>
             </div>
         </nav>
@@ -77,15 +90,50 @@
         <div class="span">
             <span id="errorSpan" class="span"></span>
         </div>
+        <div class="ms-auto p-3">
+            <button class="btn btn-primary" id="AddProduct"><a href="addProduct.php" class="text-white text-decoration-none">Add Product</a></button>
+        </div>
     </div>
         <hr>
-    <div class="container">
-        <div id="productList" class="">
+    <div class="container-sm" id="productList1">
+        <div id="productList" class="container">
 
+        <?php
+                    include('server.php');
+                    $sql = "select * from productdetails";
+                    $result = $conn->query($sql);
+                    $conn->close();
+                     if($result){
+                        while($row = mysqli_fetch_array($result)){
+                            $uid= $row['id'];
+                             $model= $row['modelname'];
+                            $imagep= $row['image'];  
+                        //  echo '<img src=data:image;base64,'.$row['$image'].' />
+                  
+        ?>
+
+            
+                            <div class="cardProduct">
+                            <div class="imgContainer">
+                            <img src="data:image/jpeg;base64,' .base64_encode($imagep).'"/>
+                            </div>
+                            <div  class="nameDiv">
+                            <h4><?php echo $model?></h4>
+                            </div>
+                            <div id="buttonss">
+                            <button id=ViewBtn class="btn btn-primary"><a href="view.php?product=<?php echo $uid ?>" class="text-white text-decoration-none">View Product</a></button>
+                            </div>
+                            </div>
+        <?php    
+        
+    }  } 
+    
+        ?>
+                        
         </div>
     </div>
     
 </body>
 <script type="text/javascript" src="Script/cartPage.js"></script>
-
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 </html>
--- server.php
+++ server.php
@@ -9,7 +9,7 @@ if($conn->connect_error){
     die ("coonect failed" .$mysql->connect_error);
 }
 else{
-    echo "Connection okayed";
+    // echo "Connection okayed";
 }
 // $sql = "Insert into registerform (`firstname`,`lastname`,`email`,`password`,`conformpassword`) values ('hello','wolrd','irfan12@gmail..com','1233','122')";
 
